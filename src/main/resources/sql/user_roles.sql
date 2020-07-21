2eedbb518bebd3a0fb890ca91a3eb2ac92b5760c
DESKTOP-KI3T2DP\fires <rpetrenko51@gmail.com>
2020-07-21 21:19:17 +0400

    Добавил заполнямость для БД

---
+++ src/main/resources/sql/user_roles.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.user_roles (username, role) VALUES ('root', 'ADMIN');
+INSERT INTO public.user_roles (username, role) VALUES ('Oleg228', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('FOX', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('Flower', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('Pashket', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('Fisher', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('Angela', 'DOCTOR');
+INSERT INTO public.user_roles (username, role) VALUES ('Allushka', 'PATIENT');
+INSERT INTO public.user_roles (username, role) VALUES ('Mitya', 'PATIENT');
+INSERT INTO public.user_roles (username, role) VALUES ('Lermontov', 'PATIENT');
+INSERT INTO public.user_roles (username, role) VALUES ('Singer', 'PATIENT');
+INSERT INTO public.user_roles (username, role) VALUES ('PoliceWoman', 'PATIENT');
---
+++ src/main/resources/sql/users.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.users (name, password) VALUES ('root', 'root');
+INSERT INTO public.users (name, password) VALUES ('Oleg228', 'NoThanks');
+INSERT INTO public.users (name, password) VALUES ('FOX', 'Qwerty');
+INSERT INTO public.users (name, password) VALUES ('Flower', 'SUN');
+INSERT INTO public.users (name, password) VALUES ('Pashket', 'Role333');
+INSERT INTO public.users (name, password) VALUES ('Fisher', 'Asdfgh');
+INSERT INTO public.users (name, password) VALUES ('Angela', 'Gelik');
+INSERT INTO public.users (name, password) VALUES ('Allushka', 'Topor789');
+INSERT INTO public.users (name, password) VALUES ('Mitya', 'IceCream');
+INSERT INTO public.users (name, password) VALUES ('Lermontov', 'Koshka');
+INSERT INTO public.users (name, password) VALUES ('Singer', 'Lolita');
+INSERT INTO public.users (name, password) VALUES ('PoliceWoman', 'HeadShot');
